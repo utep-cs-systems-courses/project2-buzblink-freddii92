aebeba9f2aabf8284bcabb710100d1ff867bd6c7
freddii92 <arodriguez242@miners.utep.edu>
2020-10-07 18:33:35 -0600

    added code in toggle red to toggle red LED

--- project/myToy/stateMachine.c
+++ project/myToy/stateMachine.c
@@ -4,7 +4,19 @@
 
 char toggle_red()
 {
-  
+  static char state = 0;
+
+  switch (state) {
+  case 0:
+    red_on = 1;
+    state = 1;
+    break;
+  case 1:
+    red_on = 0;
+    state = 0;
+    break;
+  }
+  return 1;
 }
 
 char toggle_green()
