3c7988b65ec03bcfddc7b250f8ff7c455ab2c686
freddii92 <arodriguez242@miners.utep.edu>
2020-10-07 18:23:23 -0600

    added code so LED can blink every half a second

--- project/myToy/wdInterruptHandler.c
+++ project/myToy/wdInterruptHandler.c
@@ -2,6 +2,10 @@
 #include "stateMachine.h"
 
 void
-__interrupt_vec(WDT_VECTOR) WDT(){
-  
+__interrupt_vec(WDT_VECTOR) WDT(){     // 250 interrupts/sec
+  static char blink_count = 0;
+  if (blink_count == 125) {
+    state_advance();
+    blink_count = 0;
+  }
 }
