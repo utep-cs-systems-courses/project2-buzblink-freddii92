e0bdd3e6b225e1aea4d604e6faa5bbe355a83f57
freddii92 <arodriguez242@miners.utep.edu>
2020-10-27 18:58:15 -0600

    trying something new. the other was not working. trying to make it simple

--- project/myToy/stateMachine.c
+++ project/myToy/stateMachine.c
@@ -4,68 +4,42 @@
 #include "buzzer.h"
 #include "switches.h"
 
-static int x = 500;
-
-char toggle_siren()           // toggle red and green LED to simulate siren
+char red_led_on()                      // turning red LED on and green LED off
 {
-  static char state = 0;
-
-  switch (state) {
-  case 0:
-    red_on = 1;
-    green_on = 0;
-    up_state();
-  case 1:
-    red_on = 1;
-    green_on = 0;
-    state++;
-    break;
-  case 2:
-    red_on = 0;
-    green_on = 1;
-    buzzer_set_period(1000);
-    state = 0;
-  }
-  return 1;
-}
-
-char toggle_lights()           // toggle red and green LED to simulate siren
-{
-  static char state = 0;
-
-  switch (state) {
-  case 0:
-    red_on = 1;
-    green_on = 1;
-    state = 1;
-    break;
-  case 1:
-    red_on = 0;
-    green_on = 0;
-    state = 0;
-    break;
-  }
-  return 1;
-}
-
-void up_state()
-{
-  buzzer_set_period(2000000/x);
-}
-
-void state_advance()        // alternate between toggling red and green LEDs
-{
-  if (switch_state_changed) {
-    toggle_siren();
-    led_changed = 1;
-    led_update();
-  }
-}
-
-void dimmer_advance()
-{
-  char changed = 0;
-  changed = toggle_lights();
-  led_changed = changed;
+  red_on = 1;
+  green_on = 0;
   led_update();
 }
+
+char green_led_on()                    // turning green LED on and red LED off
+{
+  red_on = 0;
+  green_on = 1;
+  led_update();
+}
+
+char both_on()                         // turning both LEDs on
+{
+  red_on = 1;
+  green_on = 1;
+  led_update();
+}
+
+char both_off()                        // turning both LEDs off
+{
+  red_on = 0;
+  green_on = 0;
+  led_update();
+}
+
+void state_advance()               // alternate between toggling red and green LEDs
+{
+  static char changed = 0;
+
+  //red_led_on();
+  //green_led_on();
+  led_changed = 1;
+  
+  led_update();
+}
+
--- project/myToy/wdInterruptHandler.c
+++ project/myToy/wdInterruptHandler.c
@@ -5,14 +5,9 @@
 void
 __interrupt_vec(WDT_VECTOR) WDT(){      // 250 interrupts/sec
   static char blink_count = 0;
-  static char dimmer_count = 0;
   
   if (++blink_count == 125) {
     state_advance();
     blink_count = 0;
-  }
-  if (++dimmer_count == 4 && switch_state_changed == 0) {
-    dimmer_advance();
-    dimmer_count = 0;
   }
 }
