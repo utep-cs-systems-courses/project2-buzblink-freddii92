4e0e9fe2de10f7d27893a2d275d74cf39c812340
freddii92 <arodriguez242@miners.utep.edu>
2020-10-29 16:29:14 -0600

    added new song for switch S3

--- project/myToy/stateMachine.c
+++ project/myToy/stateMachine.c
@@ -105,3 +105,65 @@ void dimmer_advance()
   
   led_update();
 }
+
+void happy_birthday()
+{
+  static char changed = 0;
+
+  led_changed = 1;
+
+  switch (changed) {
+  case 0: buzzer_set_period(G); red_led_on();    changed++; break;
+  case 1: buzzer_set_period(0); both_off();      changed++; break;
+  case 2: buzzer_set_period(G); red_led_on();    changed++; break;
+  case 3: buzzer_set_period(A); green_led_on();  changed++; break;
+  case 4: buzzer_set_period(0); both_off();      changed++; break;
+  case 5: buzzer_set_period(G); green_led_on();  changed++; break;
+  case 6: buzzer_set_period(0); both_off();      changed++; break;
+  case 7: buzzer_set_period(C); both_on();       changed++; break;
+  case 8: buzzer_set_period(0); both_off();      changed++; break;
+  case 9: buzzer_set_period(B); both_on();      changed++; break;
+  case 10: changed++; break;
+  case 11: buzzer_set_period(0); both_off();     changed++; break;
+  case 12: buzzer_set_period(G); red_led_on();   changed++; break;
+  case 13: buzzer_set_period(0); both_off();     changed++; break;
+  case 14: buzzer_set_period(G); red_led_on();   changed++; break;
+  case 15: buzzer_set_period(A); green_led_on(); changed++; break;
+  case 16: buzzer_set_period(0); both_off();     changed++; break;
+  case 17: buzzer_set_period(G); green_led_on(); changed++; break;
+  case 18: buzzer_set_period(0); both_off();     changed++; break;
+  case 19: buzzer_set_period(D); both_on();      changed++; break;
+  case 20: buzzer_set_period(0); both_off();     changed++; break;
+  case 21: buzzer_set_period(C); both_on();      changed++; break;
+  case 22: changed++; break;
+  case 23: buzzer_set_period(0); both_off();     changed++; break;
+  case 24: buzzer_set_period(G); red_led_on();   changed++; break;
+  case 25: buzzer_set_period(0); both_off();     changed++; break;
+  case 26: buzzer_set_period(G); red_led_on();   changed++; break;
+  case 27: buzzer_set_period(G); green_led_on(); changed++; break;
+  case 28: buzzer_set_period(0); both_off();     changed++; break;
+  case 29: buzzer_set_period(E); green_led_on(); changed++; break;
+  case 30: buzzer_set_period(0); both_off();     changed++; break;
+  case 31: buzzer_set_period(C); both_on();      changed++; break;
+  case 32: buzzer_set_period(0); both_off();     changed++; break;
+  case 33: buzzer_set_period(B); both_on();      changed++; break;
+  case 34: buzzer_set_period(0); both_off();     changed++; break;
+  case 35: buzzer_set_period(A); both_on();      changed++; break;
+  case 36: changed++; break;
+  case 37: buzzer_set_period(0); both_off();     changed++; break;
+  case 38: buzzer_set_period(F); red_led_on();   changed++; break;
+  case 39: buzzer_set_period(0); both_off();     changed++; break;
+  case 40: buzzer_set_period(F); red_led_on();   changed++; break;
+  case 41: buzzer_set_period(E); green_led_on(); changed++; break;
+  case 42: buzzer_set_period(0); both_off();     changed++; break;
+  case 43: buzzer_set_period(C); green_led_on(); changed++; break;
+  case 44: buzzer_set_period(0); both_off();     changed++; break;
+  case 45: buzzer_set_period(D); both_on();      changed++; break;
+  case 46: buzzer_set_period(0); both_off();     changed++; break;
+  case 47: buzzer_set_period(C); both_on();      changed++; break;
+  case 48: changed++; break;
+  case 49: buzzer_set_period(0); both_off;       changed = 0; break;
+  }
+
+  led_update();
+}
--- project/myToy/switches.c
+++ project/myToy/switches.c
@@ -5,6 +5,7 @@
 #include "buzzer.h"
 
 char switch_state_down, switch_state_changed;         // effectively boolean
+char speed;
 
 static char switch_update_interrupt_sense()
 {
@@ -29,26 +30,26 @@ void switch_interrupt_handler()
 {
   char p2val = switch_update_interrupt_sense();
   
-  if (p2val & SW1) {
+  if (p2val & SW1 && p2val & SW2 && p2val & SW3) {
     idle_state();
     switch_state_down = 0;
   }
-  else {
+  else if (!(p2val & SW1)) {
+    speed = 80;
     twinkle_advance();
     switch_state_down = 1;
   }
-  switch_state_changed = 1;
-  led_update();
-}
-
-void switch_interrupt_handler_SW2()
-{
-  char p2val = switch_update_interrupt_sense();
-  
-  if (!(p2val & SW2)) {
+  else if (!(p2val & SW2)) {
+    speed = 4;
     dimmer_advance();
     switch_state_down = 1;
   }
+  else if (!(p2val & SW3)) {
+    speed = 80;
+    happy_birthday();
+    switch_state_down = 1;
+  }
+  
   switch_state_changed = 1;
   led_update();
 }
