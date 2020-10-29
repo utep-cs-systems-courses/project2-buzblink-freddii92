ab8b839721498bc1e0a18d4c4dd37885c6409b15
freddii92 <arodriguez242@miners.utep.edu>
2020-10-28 18:53:45 -0600

    added buttons from P2 and a new switch_interrupt function for button S2

--- project/myToy/switches.c
+++ project/myToy/switches.c
@@ -1,32 +1,54 @@
 #include <msp430.h>
 #include "switches.h"
 #include "led.h"
+#include "stateMachine.h"
+#include "buzzer.h"
 
 char switch_state_down, switch_state_changed;         // effectively boolean
 
 static char switch_update_interrupt_sense()
 {
-  char p1val = P1IN;
+  char p2val = P2IN;
   /* update switch interrupt to detect changes from current buttons */
-  P1IES |= (p1val & SWITCHES);                        // if switch up, sense down
-  P1IES &= (p1val | ~SWITCHES);                       // if switch down, sense up
-  return p1val;
+  P2IES |= (p2val & SWITCHES);                        // if switch up, sense down
+  P2IES &= (p2val | ~SWITCHES);                       // if switch down, sense up
+  return p2val;
 }
 
 void switch_init()                                    // setup switch
 {
-  P1REN |= SWITCHES;                                  // enables resistors for switches
-  P1IE |= SWITCHES;                                   // enables interrupts from switches
-  P1OUT |= SWITCHES;                                  // pull-ups for switches
-  P1DIR &= ~SWITCHES;                                 // set switches' bits for input
+  P2REN |= SWITCHES;                                  // enables resistors for switches
+  P2IE |= SWITCHES;                                   // enables interrupts from switches
+  P2OUT |= SWITCHES;                                  // pull-ups for switches
+  P2DIR &= ~SWITCHES;                                 // set switches' bits for input
   switch_update_interrupt_sense();
   led_update();
 }
 
 void switch_interrupt_handler()
 {
-  char p1val = switch_update_interrupt_sense();       
-  switch_state_down = (p1val & SW1) ? 0 : 1;          // 0 when SW1 is up
+  char p2val = switch_update_interrupt_sense();
+  
+  if (p2val & SW1) {
+    idle_state();
+    switch_state_down = 0;
+  }
+  else {
+    twinkle_advance();
+    switch_state_down = 1;
+  }
+  switch_state_changed = 1;
+  led_update();
+}
+
+void switch_interrupt_handler_SW2()
+{
+  char p2val = switch_update_interrupt_sense();
+  
+  if (!(p2val & SW2)) {
+    dimmer_advance();
+    switch_state_down = 1;
+  }
   switch_state_changed = 1;
   led_update();
 }
--- project/myToy/switches.h
+++ project/myToy/switches.h
@@ -1,8 +1,11 @@
 #ifndef switches_included
 #define switches_included
 
-#define SW1 BIT3                                         // switch is p1.3
-#define SWITCHES SW1                                     // only 1 switch on this board
+#define SW1 BIT0                                         // switch S1 is P2.0
+#define SW2 BIT1                                         // switch S2 is P2.1
+#define SW3 BIT2                                         // switch S3 is P2.2
+#define SW4 BIT3                                         // switch S4 is P2.3
+#define SWITCHES (SW1 | SW2 | SW3 | SW4)                 // all switches
 
 void switch_init();
 void switch_interrupt_handler();
